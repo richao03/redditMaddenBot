b3b489ebfa8b0f8f8fef9ee4989524a367de7b42
Richard Chao <r.chao03@gmail.com>
2019-05-06 14:35:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Snoowrap from 'snoowrap';
 
 function App() {
+  const Reddit = new Snoowrap({
+    userAgent: 'reddit-bot-example-node',
+    clientId: ' CLIENTID',
+    clientSecret: 'CLIENTSECRET',
+    username: 'userNameToPost',
+    password: 'PasswordForUserNameToPost'
+  });
+
+  let replyAnswer =
+    "Instead of making EA rich year after year, just buy Madden off of craigslist or ebay 2nd hand, this way EA won't be able to make money off of you";
+
+    Reddit.getNewComments('madden')
+    .then(async res => {
+      for (let i = 0; i < res.length; i++) {
+        if (
+          res[i].body.includes('buy madden') ||
+          res[i].body.includes('buying madden')
+        ) {
+          //check if bot already replied
+          return res[i];
+        }
+      }
+    })
+    .then(async foundComment => {
+      let commentToReply = await foundComment.replies.fetchAll().then(res => {
+        for (let j = 0; j < res.length; j++) {
+          if (res[j].body.includes(replyAnswer)) {
+            return false;
+          }
+        }
+        return foundComment;
+      });
+      return commentToReply;
+    })
+    .then(commentToReply => {
+      if (commentToReply) {
+        Reddit.getComment(commentToReply.id).reply(replyAnswer);
+      } else {
+        return;
+      }
+    });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +57,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
